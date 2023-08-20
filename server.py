cfd19766d4c6de7219b86fe2a634642746d7bf69
joneswang <jonescwang@gmail.com>
2023-08-20 14:56:40 -0400

    gpt fixed version

--- server.py
+++ server.py
@@ -9,127 +9,137 @@ HOST, PORT = '', 8888
 
 Environment = collections.namedtuple('Environment', ['args', 'method', 'body'])
 
+
 class Server(object):
 
-  address_family = socket.AF_INET
-  socket_type = socket.SOCK_STREAM
-  request_queue_size = 100
+    address_family = socket.AF_INET
+    socket_type = socket.SOCK_STREAM
+    request_queue_size = 100
 
-  def __init__(self, server_address, callback):
-    self.callback = callback
+    def __init__(self, server_address, callback):
+        self.callback = callback
 
-    self.listen_socket = socket.socket(self.address_family, self.socket_type)
-    self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    self.listen_socket.bind(server_address)
-    self.listen_socket.listen(self.request_queue_size)
+        self.listen_socket = socket.socket(
+            self.address_family, self.socket_type)
+        self.listen_socket.setsockopt(
+            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listen_socket.bind(server_address)
+        self.listen_socket.listen(self.request_queue_size)
 
-    # Get server host name and port
-    host, port = self.listen_socket.getsockname()[:2]
-    self.server_name = socket.getfqdn(host)
-    self.server_port = port
+        # Get server host name and port
+        host, port = self.listen_socket.getsockname()[:2]
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
 
-  def ServeForever(self, verbose=False):
-    while True:
-      client_connection, client_address = self.listen_socket.accept()
-      process = multiprocessing.Process(target=_Handle, args=(
-        self.listen_socket, client_connection, self.callback, verbose))
-      process.start()
-      client_connection.close()
+    def ServeForever(self, verbose=False):
+        while True:
+            client_connection, client_address = self.listen_socket.accept()
+            process = multiprocessing.Process(target=_Handle, args=(
+                self.listen_socket, client_connection, self.callback, verbose))
+            process.start()
+            client_connection.close()
+
 
 def _Handle(listen_socket, client_connection, callback, verbose):
-  listen_socket.close()
-  handler = Handler(client_connection, callback, verbose)
-  handler.Handle()
+    listen_socket.close()
+    handler = Handler(client_connection, callback, verbose)
+    handler.Handle()
+
 
 class Handler(object):
 
-  def __init__(self, client_connection, callback, verbose=False):
-    self.client_connection = client_connection
-    self.callback = callback
-    self.verbose = verbose
-    self.handled = False
-    self.set_headers = False
+    def __init__(self, client_connection, callback, verbose=False):
+        self.client_connection = client_connection
+        self.callback = callback
+        self.verbose = verbose
+        self.handled = False
+        self.set_headers = False
 
-  def Handle(self):
-    if self.handled:
-      return
+    def Handle(self):
+        if self.handled:
+            return
 
-    self.request = self.client_connection.recv(1024)
-    if self.verbose:
-      print PrefixLinesWith(self.request, '(%d) >' % os.getpid())
+        self.request = self.client_connection.recv(1024)
+        if self.verbose:
+            print(PrefixLinesWith(self.request.decode(
+                'utf-8'), '(%d) >' % os.getpid()))
 
-    self._ParseRequest()
+        self._ParseRequest()
 
-    env = Environment(args=self.args, method=self.method, body=self.body)
-    
-    self.data = self.callback(self.path, env, self._StartResponse)
-    self._Finish()
+        env = Environment(args=self.args, method=self.method, body=self.body)
 
-  def _StartResponse(self, status, response_headers):
-    self.set_headers = True
-    server_headers = []
-    self.headers = server_headers + response_headers
-    self.status = status
+        self.data = self.callback(self.path, env, self._StartResponse)
+        self._Finish()
 
-  def _ParseRequest(self):
-    lines = [line.rstrip('\r\n') for line in self.request.splitlines()]
-    self.method, path_and_args, self.version = lines[0].split()
-    self._ParsePathAndArgs(path_and_args)
+    def _StartResponse(self, status, response_headers):
+        self.set_headers = True
+        server_headers = []
+        self.headers = server_headers + response_headers
+        self.status = status
 
-    empty = lines.index('')
-    self.body = '\n'.join(lines[(empty+1):])
+    def _ParseRequest(self):
+        lines = [line.rstrip('\r\n')
+                 for line in self.request.decode('utf-8').splitlines()]
+        self.method, path_and_args, self.version = lines[0].split()
+        self._ParsePathAndArgs(path_and_args)
 
+        empty = lines.index('')
+        self.body = '\n'.join(lines[(empty+1):])
 
-  def _ParsePathAndArgs(self, path_and_args):
-    path_and_args_str = path_and_args.split('?', 1)
-    self.path = path_and_args_str[0]
-    if len(path_and_args_str) == 1:
-      self.args = {}
-      return
-    args_str = path_and_args_str[1]
-    args_and_values = args_str.split('&')
-    self.args = dict(_ParseArgAndValue(arg_value)
-                     for arg_value in args_and_values)
+    def _ParsePathAndArgs(self, path_and_args):
+        path_and_args_str = path_and_args.split('?', 1)
+        self.path = path_and_args_str[0]
+        if len(path_and_args_str) == 1:
+            self.args = {}
+            return
+        args_str = path_and_args_str[1]
+        args_and_values = args_str.split('&')
+        self.args = dict(_ParseArgAndValue(arg_value)
+                         for arg_value in args_and_values)
 
-  def _Finish(self):
-    contents = ''.join(data_part for data_part in self.data)
+    def _Finish(self):
+        contents = ''.join(data_part for data_part in self.data)
 
-    if not self.set_headers:
-      raise ValueError('Must call start_response before returning.')
+        if not self.set_headers:
+            raise ValueError('Must call start_response before returning.')
 
-    response = 'HTTP/1.1 {status}\r\n'.format(status=self.status)
-    for header in self.headers:
-      response += '{0}: {1}\r\n'.format(*header)
-    response += '\r\n'
-    response += contents
+        response = 'HTTP/1.1 {status}\r\n'.format(status=self.status)
+        for header in self.headers:
+            response += '{0}: {1}\r\n'.format(*header)
+        response += '\r\n'
+        response += contents
 
-    if self.verbose:
-      content = response
-      if self.path.endswith(".png"):
-        content = "BINARY FILE: %s" % self.path
-      print PrefixLinesWith(content, '(%d) <' % os.getpid())
+        if self.verbose:
+            content = response
+            if self.path.endswith(".png"):
+                content = "BINARY FILE: %s" % self.path
+            print(PrefixLinesWith(content, '(%d) <' % os.getpid()))
 
-    try:
-      self.client_connection.sendall(response)
-    finally:
-      self.client_connection.close()
+        try:
+            self.client_connection.sendall(response.encode('utf-8'))
+        finally:
+            self.client_connection.close()
+
 
 def _ParseArgAndValue(arg_value):
-  parts = arg_value.split('=', 1)
-  if len(parts) == 1:
-    return [arg_value, 'True']
-  return parts
+    parts = arg_value.split('=', 1)
+    if len(parts) == 1:
+        return [arg_value, 'True']
+    return parts
+
 
 def PrefixLinesWith(text, prefix):
-  return ''.join('{prefix} {line}\n'.format(prefix=prefix, line=line)
-                 for line in text.splitlines())
+    return ''.join('{prefix} {line}\n'.format(prefix=prefix, line=line)
+                   for line in text.splitlines())
 
-def DoResponse(path, args, start_response):
-  start_response(200, [])
-  yield 'Path: %s\n' % path
-  for arg, value in args.iteritems():
-    yield '%s=%s\n' % (arg, value)
+
+def DoResponse(path, env, start_response):
+    start_response(200, [('Content-Type', 'text/html')])
+    with open("static/index.html", "r") as file:
+        content = file.read()
+    yield content
+
 
 if __name__ == '__main__':
-  server = Server(('', 8888), DoResponse)
-  server.ServeForever()
+    server = Server(('', 8888), DoResponse)
+    server.ServeForever()
--- shogi_server.py
+++ shogi_server.py
@@ -1,371 +1,136 @@
 import collections
 import json
 import logging
-import multiprocessing
 import os
 import server
 import shogi
 import shogi_ai
 import random
 
-NUM_THREADS = 20
-
-
-class Sections:
-  WORKER = "worker"
-  MATCH_MAKER = "match_maker"
-  GAME_MOTHER = "game_mother"
-  AI_MOTHER = "ai_mother"
-
 
 class ContentTypes:
-  PLAIN = ('Content-Type', 'text/plain')
-  JSON = ('Content-Type', 'application/json')
-  HTML = ('Content-Type', 'text/html')
-  CSS = ('Content-Type', 'text/css')
-  JAVASCRIPT = ('Content-Type', 'text/javascript')
-
-Player = collections.namedtuple('Player', ['order', 'is_human'])
-
-GameState = collections.namedtuple('GameState', ['game', 'players'])
-
-Address = collections.namedtuple('Address', ['section', 'index'])
+    PLAIN = ('Content-Type', 'text/plain')
+    JSON = ('Content-Type', 'application/json')
+    HTML = ('Content-Type', 'text/html')
+    CSS = ('Content-Type', 'text/css')
+    JAVASCRIPT = ('Content-Type', 'text/javascript')
 
 
-class QueuePool(object):
-
-  def __init__(self):
-    self.queues = []
-    self.access_queue = multiprocessing.Queue()
-
-  def AddQueues(self, num_queues):
-    for i in range(num_queues):
-      self.queues.append(multiprocessing.Queue())
-      self.access_queue.put(i)
-
-  def Obtain(self):
-    index = self.access_queue.get()
-    return index, self.queues[index]
-
-  def Done(self, index):
-    self.access_queue.put(index)
-
-  def Get(self, index):
-    return self.queues[index]
+class GameState:
+    def __init__(self, game, players):
+        self.game = game
+        self.players = players
 
 
-class Connections(object):
-  
-  REGISTRY = {}
-
-  @classmethod
-  def AddQueues(cls, section, amount):
-    assert section not in cls.REGISTRY
-    cls.REGISTRY[section] = QueuePool()
-    cls.REGISTRY[section].AddQueues(amount)
-
-  @classmethod
-  def Get(cls, address):
-    section, index = address
-    return cls.REGISTRY[section].Get(index)
-
-  @classmethod
-  def Obtain(cls, section):
-    index, queue = cls.REGISTRY[section].Obtain()
-    return Address(section, index), queue
-
-  @classmethod
-  def Done(cls, address):
-    section, index = address
-    return cls.REGISTRY[section].Done(index)
+class Player:
+    def __init__(self, order, is_human):
+        self.order = order
+        self.is_human = is_human
 
 
-class Tasker(object):
+class Handler:
+    def __init__(self, path, env, start_response):
+        self.path = path
+        self.env = env
+        self.start_response = start_response
+        self.code = 200
+        self.content_type = ContentTypes.JSON
+        self.headers = []
 
-  num_instances = 0
-  section = None
+    def Handle(self):
+        if self.path == "start_game":
+            return self.StartGame()
 
-  @classmethod
-  def SpawnAll(cls):
-    for _ in xrange(cls.num_instances):
-      instance = cls()
-      process = multiprocessing.Process(target=instance.ServeForever)
-      process.start()
+        if self.path == "get_game_status":
+            player_id = int(self.env.args['player'])
+            return self.GetStatus(player_id)
 
-  @classmethod
-  def SetPoolSize(cls, num):
-    Connections.AddQueues(cls.section, num)
-    cls.num_instances = num
+        if self.path == "move":
+            player_id = int(self.env.args['player'])
+            move_from = self.env.args['from']
+            move_to = self.env.args['to']
+            return self.Move(player_id, move_from, move_to)
 
-  @classmethod
-  def GetAnyQueue(cls, *args):
-    # Default to random distrubition
-    return Connections.Get((cls.section, random.randrange(cls.num_instances)))
+        return self.ReadFile()
 
-  def __init__(self):
-    self.address, self.tasks = Connections.Obtain(self.section)
-    self.functions = {}
+    def StartGame(self):
+        game = shogi.Game()
+        player_1 = Player(shogi.PLAYER1, True)
+        player_2 = Player(shogi.PLAYER2, True)
+        players = {1: player_1, 2: player_2}
+        state = GameState(game, players)
+        return self.GetStatus(1)
 
-  def AddFunction(self, name, function):
-    self.functions[name] = function
+    def GetStatus(self, player_id):
+        state = self.game_state
+        game = state.game
+        player = state.players[player_id].order
 
-  def ServeForever(self):
-    while True:
-      task = self.tasks.get()
-      task_type = task[0]
-      args = task[1:]
-      if task_type not in self.functions:
-        print "Task %s not a valid task type!" % task_type
-        continue
-      self.functions[task_type](*args)
+        payload = {}
+        payload['board'] = game.board
+        payload['status'] = game.status[player]
+        payload['current_player'] = game.player
+        if game.player == player:
+            payload['moves'] = shogi.PossibleMoves(game.board, player)
+        return json.dumps(payload)
+
+    def Move(self, player_id, move_from, move_to):
+        state = self.game_state
+        game = state.game
+        player = state.players[player_id].order
+
+        if player != game.player:
+            return self.GetStatus(player_id)
+
+        boards = shogi.PossibleMoves(game.board, game.player)
+        if move_to not in boards[move_from]:
+            return self.GetStatus(player_id)
+
+        next_board = boards[move_from][move_to]
+        game.UpdateBoard(next_board)
+        return self.GetStatus(player_id)
+
+    def ReadFile(self):
+        if self.path == '':
+            self.path = 'index.html'
+
+        file_path = 'static/' + self.path
+        if os.path.isfile(file_path):
+            if file_path.endswith(".html"):
+                self.content_type = ContentTypes.HTML
+            if file_path.endswith(".css"):
+                self.content_type = ContentTypes.CSS
+            if file_path.endswith(".js"):
+                self.content_type = ContentTypes.JAVASCRIPT
+            with open(file_path, 'r') as f:
+                return f.read()
+        else:
+            self.code = 404
+            self.content_type = ContentTypes.HTML
+            return "<h1>404 Page Not Found<h1>"
 
 
 def _EntryPoint(path, env, start_response):
-  assert path[0] == '/'
-  path = path[1:]
-  address, queue = Connections.Obtain(Sections.WORKER)
-  handler = Handler(path, env, address, queue)
-  try:
-    contents = handler.Handle()
-  except Exception as e:  # We need to be as broad as possible here.
-    logging.exception("Could not handle a request to %s" % path)
-    start_response(500, [ContentTypes.HTML])
-    return [("<h1>500 Internal Server Error</h1>" +
-             "<p>We're sorry, the server was unable to process your " +
-             "request.</p>")]
-  finally:
-    Connections.Done(address)
+    assert path[0] == '/'
+    path = path[1:]
+    handler = Handler(path, env, start_response)
+    try:
+        contents = handler.Handle()
+    except Exception as e:
+        logging.exception("Could not handle a request to %s" % path)
+        start_response(500, [ContentTypes.HTML])
+        return [("<h1>500 Internal Server Error</h1>" +
+                 "<p>We're sorry, the server was unable to process your " +
+                 "request.</p>")]
 
-  headers = [handler.content_type] + handler.headers
-  start_response(handler.code, headers)
-  return [contents]
-
-class Handler(object):
-
-  def __init__(self, path, env, address, queue):
-    self.path = path
-    self.env = env
-    self.queue = queue
-    self.address = address
-    self.code = 200
-    self.content_type = ContentTypes.JSON
-    self.headers = []
-
-  def Handle(self):
-    if self.path=="start_game": 
-      return self.StartGame()
-
-    if self.path=="start_ai_game":
-      return self.StartAIGame()
-
-    if self.path=="get_game_status":
-      player_id = int(self.env.args['player'])
-      return self.GetStatus(player_id)
-
-    if self.path=="move":
-      player_id = int(self.env.args['player'])
-      move_from = self.env.args['from']
-      move_to = self.env.args['to']
-      return self.Move(player_id, move_from, move_to)
-
-    if self.path=="wait_for_my_turn":
-      player_id = int(self.env.args['player'])
-      return self.WaitForTurn(player_id)
-
-    return self.ReadFile()
-
-  def StartGame(self):
-    MatchMaker.GetAnyQueue().put(("match", self.address))
-    player_id, player = self.queue.get()
-    payload = {'player_id': player_id, 'player': player }
-    return json.dumps(payload)
-
-  def StartAIGame(self):
-    GameMother.GetAnyQueue().put(("new_board", self.address, [True, False]))
-    player_id, ai_player_id = self.queue.get()
-    payload = {'player_id': player_id, 'player': shogi.PLAYER1 }
-    self._DoAITurnIfNeeded(player_id)
-    return json.dumps(payload)
-
-  def GetStatus(self, player_id):
-    GameMother.GetQueue(player_id).put(("get_state", self.address, player_id))
-    state = self.queue.get()
-    game = state.game
-    player = state.players[player_id].order
-
-    payload = {}
-    payload['board'] = game.board
-    payload['status'] = game.status[player]
-    payload['current_player'] = game.player
-    if game.player == player:
-      payload['moves'] = shogi.PossibleMoves(game.board, player)
-    return json.dumps(payload)
-
-  def Move(self, player_id, move_from, move_to):
-    GameMother.GetQueue(player_id).put(("get_state", self.address, player_id))
-    state = self.queue.get()
-    game = state.game
-    player = state.players[player_id].order
-
-    if player != game.player:
-      return self.GetStatus(player_id)
-
-    boards = shogi.PossibleMoves(game.board, game.player)
-    if move_to not in boards[move_from]:
-      return self.GetStatus(player_id)
-
-    next_board = boards[move_from][move_to]
-    GameMother.GetQueue(player_id).put(("put_board", self.address, player_id, next_board))
-    ack = self.queue.get()
-    self._DoAITurnIfNeeded(player_id)
-    return self.GetStatus(player_id)
-
-  def WaitForTurn(self, player_id):
-    GameMother.GetQueue(player_id).put(("wake_at_my_turn", self.address, player_id))
-    # TODO(npat): timeout in ~30 seconds and clear the waking.
-    ack = self.queue.get()
-    return self.GetStatus(player_id)
-
-  def _DoAITurnIfNeeded(self, player_id):
-    GameMother.GetQueue(player_id).put(("get_state", self.address, player_id))
-    state = self.queue.get()
-    current_player = state.game.player
-    for other_player_id, player in state.players.iteritems():
-      if player.is_human or player.order != current_player:
-        continue
-      ai_address, ai_queue = Connections.Obtain(Sections.WORKER)
-      process = multiprocessing.Process(
-          target=DoAITurn, args=(ai_address, ai_queue, other_player_id))
-      process.start()
-
-  def ReadFile(self):
-    if self.path == '':
-      self.path = 'index.html'
-
-    file_path = 'static/' + self.path
-    if os.path.isfile(file_path):
-      if file_path.endswith(".html"): self.content_type = ContentTypes.HTML
-      if file_path.endswith(".css"): self.content_type = ContentTypes.CSS
-      if file_path.endswith(".js"): self.content_type = ContentTypes.JAVASCRIPT
-      with open(file_path, 'r') as f:
-        return f.read()
-    else:
-      self.code = 404
-      self.content_type = ContentTypes.HTML
-      return "<h1>404 Page Not Found<h1>"
-
-
-class GameMother(Tasker):
-
-  section = Sections.GAME_MOTHER
-
-  @classmethod
-  def GetQueue(cls, player_id):
-    index = player_id % cls.num_instances
-    return Connections.Get((cls.section, index))
-
-  def __init__(self):
-    super(GameMother, self).__init__()
-    self.game_states = {}
-    self.waiting = collections.defaultdict(list)
-    self.AddFunction("get_state", self.GetState)
-    self.AddFunction("put_board", self.PutBoard)
-    self.AddFunction("wake_at_my_turn", self.WakeAtTurn)
-    self.AddFunction("new_board", self.NewBoard)
-
-  def GetState(self, caller, player_id):
-    if not player_id in self.game_states:
-      Connections.Get(caller).put(False)
-      return
-    state = self.game_states[player_id]
-    Connections.Get(caller).put(state)
-
-  def PutBoard(self, caller, player_id, board):
-    game = self.game_states[player_id].game
-    game.UpdateBoard(board)
-    Connections.Get(caller).put(True)
-
-    waiters = self.waiting[player_id]
-    for waiter in waiters:
-      Connections.Get(waiter).put(True)
-    del self.waiting[player_id]
-
-  def WakeAtTurn(self, caller, player_id):
-    state = self.game_states[player_id]
-    game = state.game
-    player = state.players[player_id].order
-
-    if game.player == player or game.is_over:
-      Connections.Get(caller).put(True)
-      return
-
-    players = state.players
-    for other_player_id in players.keys():
-      if other_player_id != player_id:
-        self.waiting[other_player_id].append(caller)
-
-  def NewBoard(self, caller, human_or_ai=None):
-    if not human_or_ai:
-      human_or_ai = [True, True]
-
-    player_1_id = random.getrandbits(32) * self.num_instances + self.address.index
-    player_2_id = random.getrandbits(32) * self.num_instances + self.address.index
-
-    game = shogi.Game()
-    players = {player_1_id: Player(shogi.PLAYER1, human_or_ai[0]), 
-               player_2_id: Player(shogi.PLAYER2, human_or_ai[1])}
-    state = GameState(game, players)
-
-    self.game_states[player_1_id] = state
-    self.game_states[player_2_id] = state
-    Connections.Get(caller).put((player_1_id, player_2_id))
-
-
-class MatchMaker(Tasker):
-
-  section = Sections.MATCH_MAKER
-
-  def __init__(self):
-    super(MatchMaker, self).__init__()
-    self.waiting = None
-    self.AddFunction('match', self.Match)
-
-  def Match(self, caller):
-    if self.waiting == None:
-      self.waiting = caller
-      return
-    temp_address, temp_queue = Connections.Obtain(Sections.WORKER)
-    GameMother.GetAnyQueue().put(('new_board', temp_address))
-    player_1_id, player_2_id = temp_queue.get()
-    Connections.Done(temp_address)
-    Connections.Get(self.waiting).put((player_1_id, shogi.PLAYER1))
-    Connections.Get(caller).put((player_2_id, shogi.PLAYER2))
-    self.waiting = None
-
-
-def DoAITurn(address, queue, player_id):
-  GameMother.GetQueue(player_id).put(('get_state', address, player_id))
-  state = queue.get()
-  assert state.players[player_id].order == state.game.player
-
-  possible_boards = shogi.Next(state.game.board, state.game.player)
-  board_chosen = shogi_ai.Mix(possible_boards, state.game.player) 
-  new_board = possible_boards[board_chosen]
-
-  GameMother.GetQueue(player_id).put(('put_board', address, player_id, new_board))
-  queue.get()
-  Connections.Done(address)
-
+    headers = [handler.content_type] + handler.headers
+    start_response(handler.code, headers)
+    return [contents]
 
 
 def SetUp():
-  MatchMaker.SetPoolSize(1)
-  GameMother.SetPoolSize(1)
-  Connections.AddQueues(Sections.WORKER, NUM_THREADS)
+    server.Server(('', 8008), _EntryPoint).ServeForever(True)
 
-  MatchMaker.SpawnAll()
-  GameMother.SpawnAll()
-  server.Server(('', 8008), _EntryPoint).ServeForever(True)
 
-if __name__=="__main__":
-  SetUp()
+if __name__ == "__main__":
+    SetUp()
