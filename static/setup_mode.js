6eecf569034f1b4175cfaad61e1a34e31caa70ca
joneswang <jonescwang@gmail.com>
2023-08-30 16:42:38 -0400

    almost done with setup mode

--- static/setup_mode.js
+++ static/setup_mode.js
@@ -149,13 +149,42 @@ function displayGameStartButtons() {
     startBtnP2.id = "startBtnP2";
     startBtnP2.style.display = ""; // 顯示按鈕
     board.appendChild(startBtnP2);
+
+    // 在這裡為按鈕添加事件監聽器
+    startBtnP1.addEventListener("click", function () {
+      startGameAsPlayer(0);
+    });
+
+    startBtnP2.addEventListener("click", function () {
+      startGameAsPlayer(1);
+    });
   } else {
     // 如果沒有兩隻獅子，則隱藏按鈕
     if (startBtnP1.parentElement) startBtnP1.style.display = "none";
     if (startBtnP2.parentElement) startBtnP2.style.display = "none";
   }
+}
 
-  // ... 其他代碼 ...
+function startGameAsPlayer(playerIndex) {
+  // 1. 設定遊戲模式
+  isSetupMode = false;
+
+  // 2. 移除按鈕
+  var startBtnP1 = document.getElementById("startBtnP1");
+  var startBtnP2 = document.getElementById("startBtnP2");
+  startBtnP1.parentNode.removeChild(startBtnP1);
+  startBtnP2.parentNode.removeChild(startBtnP2);
+
+  // 3. 初始化遊戲狀態
+  var currentPlayer = playerIndex; // 0 for Player 1 and 1 for Player 2
+
+  // 4. 啟用遊戲互動
+  enableGameInteractions(currentPlayer);
+}
+
+function enableGameInteractions(currentPlayer) {
+  // 這裡你可以啟用所有與遊戲互動相關的功能，
+  // 如根據當前玩家顯示可能的移動、處理玩家的移動、檢查遊戲是否結束等。
 }
 
 // 一旦棋盤和棋子被創建，調用以啟用拖放功能
